4b8841e89996a2d3bf121e309adaea3690b6fca8
Mr.Satvik Kishore <satvik.k@sjri.res.in>
2019-09-26 17:06:04 +0530

    refined microbenchmarking

--- r/tests.R
+++ r/tests.R
@@ -1,7 +1,8 @@
+library(Rcpp)
 library(magrittr)
 library(microbenchmark)
 set.seed(84)
-mat = matrix(rnorm(10000), ncol = 100, nrow = 100)
+mat = matrix(rnorm(600^2), ncol = 600, nrow = 600)
 mat = mat %*% t(mat)
 #base::chol pivot=F----
 l_cho = chol(mat)
@@ -19,14 +20,14 @@ norm((l_cho %*% t(l_cho)) - mat, "F")
 
 #microbenchmarking ----
 set.seed(42)
-mat2 = matrix(rnorm(500^2), ncol = 500, nrow = 500)
-mat2 = mat2 %*% t(mat2)
 mbc = list()
-ns = c(50,100,200,300)
+nns = c(2:8)*100
+ns = lapply(nns, function(z) matrix(rnorm(z^2), nrow = z) %>% (function(zz) zz %*% t(zz)))
 for(i in 1:length(ns)){
-  n = ns[i]
-  mat3 = mat2[1:n,1:n]
+  print(i)
+  mat3 = ns[[i]]
   mbc[[i]] = microbenchmark(chol(mat3), cholesky_r(mat3), cholesky_cpp(mat3)) %>% summary
 }
-mbc_df = sapply(mbc, `[[`, "median") %>% t %>% cbind(ns,.) %>% as.data.frame %>% `names<-`(c("n", "base", "r", "cpp"))
-mbc_df2 = cbind(mbc_df, sapply(mbc_df[,-1], function(z) z/(mbc_df$n^2)) %>% `names<-`(paste0(names(mbc_df),"_adj")))
+mbc_df = sapply(mbc, `[[`, "median") %>% t %>% cbind(nns,.) %>% as.data.frame %>% `names<-`(c("n", "base", "r", "cpp"))
+mbc_df2 = cbind(mbc_df, sapply(mbc_df[,-1], function(z) z/(mbc_df$n^2)))
+names(mbc_df2) = c("n",'base','r','cpp','base_adj','r_adj','cpp_adj')
